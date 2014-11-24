e877c2ddd82c2c5b385421f0aaba572a099638fc
Mansimran Singh <me@mansimransingh.com>
2014-11-24 18:32:29 +0000

    print hello world n times
    first argument is program name?
    second arg (ind 1) shall be the number of times we're gonna print shit
    
    default to printing it once

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h> // for atoi
 
 int main(int argc, char** argv){
-    puts("Hello, world!");
+    int n = 1;
+    if(argc > 1){ //first argument is program name
+        n = atoi(argv[1]); //read the first arg as int
+    }
+
+    while(n > 0){
+        puts("Hello, world!");
+        n--;
+    }
     return 0;
 }
