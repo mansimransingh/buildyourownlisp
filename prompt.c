73fd2f336039c934e8d1ccbaaf5a517a4f9fa55f
Mansimran Singh <me@mansimransingh.com>
2014-12-19 16:36:25 +0000

    added editline

--- prompt.c
+++ prompt.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-const int buffer_size = 2048;
-static char input[buffer_size];
+#include <stdlib.h>
+
+#include <editline/readline.h>
 
 int main(int argc, char** argv){
     puts("Lispy");
 
     while(1){
-        fputs("lispy> ", stdout);
+        char* input = readline("lispy> ");
 
-        fgets(input, buffer_size, stdin);
+        add_history(input);
 
-        printf("No? %s", input);
+        printf("No? %s\n", input);
+
+        free(input);
     }
 
     return 0;
