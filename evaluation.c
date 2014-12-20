629cb8e82feadb1049861866277674edbb12e04e
Mansimran Singh <me@mansimransingh.com>
2014-12-20 13:29:59 +0000

    Eval

--- evaluation.c
+++ evaluation.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "mpc.h"
 
 #ifdef _WIN32
-#include <string.h>
 
 static char buffer[2048];
 
 char* readline(char* prompt){
     fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
+    fgets(bugger, 2048, stdin);
     char* cpy = malloc(strlen(buffer)+1);
     strcpy(cpy, buffer);
     cpy[strlen(cpy)-1] = '\0';
@@ -23,13 +19,40 @@ void add_history(char* unused){}
 #include <editline/readline.h>
 #endif
 
-int main(int argc, char** argv){
-    mpc_parser_t* Number    = mpc_new("number");
-    mpc_parser_t* Operator  = mpc_new("operator");
-    mpc_parser_t* Expr      = mpc_new("expr");
-    mpc_parser_t* Lispy     = mpc_new("lispy");
+long eval_op(long x, char* op, long y){
+    if(strcmp(op, "+") == 0){ return x+y; }
+    if(strcmp(op, "-") == 0){ return x-y; }
+    if(strcmp(op, "*") == 0){ return x*y; }
+    if(strcmp(op, "/") == 0){ return x/y; }
+    return 0;
+}
 
-    mpca_lang(MPCA_LANG_DEFAULT,
+long eval(mpc_ast_t* t){
+
+    if(strstr(t->tag, "number")){
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+int main(int argc, char** argv){
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT, 
             "                                                       \
              number     : /-?[0-9]+/ ;                              \
              operator   : '+' | '-' | '*' | '/' ;                   \
@@ -37,22 +60,20 @@ int main(int argc, char** argv){
              lispy      : /^/ <operator> <expr>+ /$/ ;              \
             ",
         Number, Operator, Expr, Lispy);
-    
+
     puts("Lispy");
+    puts("Press Ctrl+c to Exit");
+    puts("--------------------------------------\n");
 
     while(1){
         char* input = readline("lispy> ");
-
         add_history(input);
 
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Lispy, &r)){
-            mpc_ast_print(r.output);
-            mpc_ast_t* a = r.output;
-            printf("Tag: %s\n", a->tag);
-            printf("Contents: %s\n", a->contents);
-            printf("Number of children %i\n", a->children_num);
 
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
@@ -60,8 +81,12 @@ int main(int argc, char** argv){
         }
 
         free(input);
+
     }
-    
+
     mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
     return 0;
+
 }
+
