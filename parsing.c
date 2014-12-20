c5b74e157f6888cd0c7779d677013080eed54240
Mansimran Singh <me@mansimransingh.com>
2014-12-20 12:11:56 +0000

    chapter 6 - parsing

--- parsing.c
+++ parsing.c
@@ -24,6 +24,20 @@ void add_history(char* unused){}
 #endif
 
 int main(int argc, char** argv){
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                       \
+             number     : /-?[0-9]+/ ;                              \
+             operator   : '+' | '-' | '*' | '/' ;                   \
+             expr       : <number> | '(' <operator> <expr>+ ')' ;   \
+             lispy      : /^/ <operator> <expr>+ /$/ ;              \
+            ",
+        Number, Operator, Expr, Lispy);
+    
     puts("Lispy");
 
     while(1){
@@ -31,10 +45,18 @@ int main(int argc, char** argv){
 
         add_history(input);
 
-        printf("No? %s\n", input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Lispy, &r)){
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
-
+    
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
